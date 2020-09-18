34a99f10a243eb5930606718d154032c04d5d6e6
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-09-18 15:58:46 +0200

    parte prima

--- js/script.js
+++ js/script.js
@@ -8,6 +8,27 @@
 
 
 $(document).ready(function() {
+  // data iniziale calendario
+  var date = "2018-01-01";
 
-  
+  var momentDate = moment(date);
+  var daysInMonth = momentDate.daysInMonth();
+
+  // template dei giorni del calendario
+  var source = $("#day-template").html();
+  var template = Handlebars.compile(source)
+
+  // faccio un ciclo per quanti sono i giorni del mese
+  for (var i = 1; i <= daysInMonth; i++) {
+    var context = {
+      "day": i,
+      "month": momentDate.format("MMMM")
+    };
+
+    // compilo il template e lo appendo
+    var html = template(context);
+    $(".days").append(html);
+
+  }
+
 });
