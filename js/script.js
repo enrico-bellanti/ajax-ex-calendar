08155b3aaa21c0a0505f5364f938df12cab32f3a
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-09-18 16:50:26 +0200

    esercizio terminato

--- js/script.js
+++ js/script.js
@@ -9,10 +9,16 @@
 
 $(document).ready(function() {
   // data iniziale calendario
-  var date = "2018-01-01";
+  var date = "2018-12-01";
 
   var momentDate = moment(date);
   var daysInMonth = momentDate.daysInMonth();
+
+  // ottengo i valori di anno e mese in Momentjs
+  var momentMonth = momentDate.month();
+  var momentYear = momentDate.year();
+
+
 
   // template dei giorni del calendario
   var source = $("#day-template").html();
@@ -20,15 +26,53 @@ $(document).ready(function() {
 
   // faccio un ciclo per quanti sono i giorni del mese
   for (var i = 1; i <= daysInMonth; i++) {
+
+    // compilo il template e lo appendo
     var context = {
       "day": i,
       "month": momentDate.format("MMMM")
     };
 
-    // compilo il template e lo appendo
     var html = template(context);
     $(".days").append(html);
+  }
+
+  $.ajax(
+   {
+    "url": "https://flynn.boolean.careers/exercises/api/holidays",
+    "data": {
+      "year": momentYear,
+      "month": momentMonth
+    },
+    "method": "GET",
+    "success": function (data) {
+      var holydayData = data.response;
+      printHolyday(holydayData);
+    },
+    "error": function (err) {
+      alert("E avvenuto un errore. "+ errore);
+    }
+  });
+
+// end document ready
+});
+
+
+// funzioni
+
+// scrivo una funzione che mi estrapoli i valori della data e del nome
+// per ogni festivita' del mese prescelto
+function printHolyday(holidays) {
+  for (var i = 0; i < holidays.length; i++) {
+    var holidayDate = holidays[i].date;
+    // trasformo la data per momentjs e ricavo il numero del mese corrispondente
+    var dayOfHoliday = moment(holidayDate).date();
+    // ricavo il nome della festivita
+    var holidayName = holidays[i].name;
+    // gli applico la classe holyday e scrivo il nome della festa nell'html
+    $(".day:nth-child("+dayOfHoliday+")").addClass("holyday");
+    $(".day:nth-child("+dayOfHoliday+")").children(".name_holiday").text(" " + holidayName);
 
   }
 
-});
+}
