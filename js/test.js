c0af7f664f02a914d89b8dfc765ae86ff539b477
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-09-20 20:14:12 +0200

    refactoring completo

--- js/test.js
+++ js/test.js
@@ -1,11 +1,3 @@
-// Descrizione:
-// Creiamo un calendario dinamico con le festività.
-// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018
-// (unici dati disponibili sull’API).
-// Milestone 1
-// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
-// API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
-
 $(document).ready(function() {
   // dichiaro variabili per settare la data
   var setYear = 2018;
@@ -27,7 +19,7 @@ $(document).ready(function() {
       printMonth(setYear, setMonth);
     }
   });
-  
+
 });
 // end document ready
 
